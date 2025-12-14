d8ca743bc48d6dbba7a43f821ab00857d5615bba
Yanni Peng <yannipeng@google.com>
2025-12-14 14:15:58 -0800

    Update create_activation_script.sh

--- create_activation_script.sh
+++ create_activation_script.sh
@@ -6,7 +6,7 @@ cat <<EOF > "$OUTPUT_FILE"
 export PROJECT_ID=$(gcloud config get-value project)
 export LOCATION=us-central1
 export MODEL_ID=gemini-2.5-flash
-export GOOGLE_GENAI_USE_VERTEXAI=1
+export GOOGLE_GENAI_USE_VERTEXAI=true
 export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
 export GOOGLE_CLOUD_LOCATION=us-central1
 # virtual environment path
