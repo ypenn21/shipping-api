e41cbc8c8bfd802b9dba74907ccc074ecd8b7280
yannipeng <yannipeng@google.com>
2025-12-14 14:45:22 -0800

    update test python folder

--- tests/test.py
+++ tests/test.py
@@ -16,34 +16,5 @@ class TestPackage(unittest.TestCase):
     def test_get_package(self):
         """Test the `get_package()` function."""
 
-        package = Package(
-        product_id=11, # Ensure that the product_id different from the sample data
-        height=10,
-        width=10,
-        depth=10,
-        weight=10,
-        special_handling_instructions="Fragile",
-        )
-
-        session = self.session_maker
-
-        session.add(package)
-        session.commit()
-
-        response = app.test_client().get("/packages/11")
-
-        self.assertEqual(response.status_code, 200)
-
-        self.assertEqual(
-            response.json,
-            {
-                "height": 10,
-                "width": 10,
-                "depth": 10,
-                "weight": 10,
-                "special_handling_instructions": "Fragile",
-            },
-        )
-
 if __name__ == "__main__":
     unittest.main()
