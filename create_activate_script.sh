b8dae670a56217f210b250ef150d771193102f49
yannipeng <yannipeng@google.com>
2025-12-15 14:43:02 -0800

    GOOGLE_CLOUD_LOCATION=global

--- create_activate_script.sh
+++ create_activate_script.sh
@@ -7,7 +7,7 @@ export PROJECT_ID=$(gcloud config get-value project)
 export LOCATION=us-central1
 export GOOGLE_GENAI_USE_VERTEXAI=true
 export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
-export GOOGLE_CLOUD_LOCATION=us-central1
+export GOOGLE_CLOUD_LOCATION=global
 # virtual environment path
 export VIRTUAL_ENV="$SHIPPING_PATH/.venv"
 export PATH="$SHIPPING_PATH/bin:$PATH"
