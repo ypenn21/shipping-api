e40016c09d4f414d837c824f11230aaa1cfb6036
yannipeng <yannipeng@google.com>
2026-01-07 14:23:00 -0500

    fix: upgrade sqlalchemy to 1.4.54 and fix deprecation warnings

--- connect_connector.py
+++ connect_connector.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.pool import StaticPool
 
 # Create an in-memory SQLite database
